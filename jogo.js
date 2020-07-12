fd5cf4503b9c28f85d6f0afa3aef3c4d22f009a2
vic515 <63172583+vic515@users.noreply.github.com>
2020-07-12 14:22:54 -0300

    Game over adicionado

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -13,11 +14,7 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
-	if(document.getElementById('mosquito')){
-		document.getElementById('mosquito').remove()
-	}
-
-
+	remover()
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 	posicaoX = posicaoX < 0 ? 0 : posicaoX
@@ -31,35 +28,62 @@ function posicaoRandomica() {
 	var mosquito = document.createElement('img')
 
 	mosquito.src = 'imagens/mosca.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = mosquitoClasse() + ' ' + mosquitoSidee()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
 	document.body.appendChild(mosquito)
 
-	var mosquitoClass = Math.random()
+	console.log(mosquito.className)
 
-	var classe1
+	if (vidas > 3) {
+		gameOver()
+	}
+	
+}
 
-	if(mosquitoClass < 1/3) {
-		mosquito.className = 'mosquito1'
-	} else if (mosquitoClass < 2/3) {
-		mosquito.className = 'mosquito2'
-	} else {
-		mosquito.className = 'mosquito3'
+function remover() {
+	if(document.getElementById('mosquito')){
+		document.getElementById('mosquito').remove()
+
+		document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+		vidas++
+	}
+}
+
+function gameOver() {
+		window.location.assign('fim_de_jogo.html')
+}
+
+function clicked() {
+	remover()
+}
+
+function mosquitoClasse() {
+	var mosquitoClass = Math.floor(Math.random() * 3)
+
+	switch (mosquitoClass) {
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:
+			return 'mosquito3'
 	}
 
-	classe1 = mosquito.className
+}
 
-	console.log(classe1)
+function mosquitoSidee() {
 	var mosquitoSide = Math.floor(Math.random() * 2)
 	switch (mosquitoSide) {
 		case 0:
-			mosquito.className = classe1 + ' ladoA'
-			break
+			return ' ladoA'
 		case 1:
-			mosquito.className = classe1 + ' ladoB'
-			break
+			return ' ladoB'
 	}
 }
+
